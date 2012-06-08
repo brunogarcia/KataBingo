4de7ec3849d512537a006c2f19da9b9635a4c114
Bruno Garcia <bruno@garciaechegaray.com>
2012-06-08 21:44:54 +0200

    Atascado en la notificación de cada numero al jugador

--- jasmine/spec/BingoTest.js
+++ jasmine/spec/BingoTest.js
@@ -1,31 +1,44 @@
 describe("En la Kata Bingo", function() {
-    var bombo;
-    var carton;
-    var jugador;
+    var bombo, carton, jugador;
 
     beforeEach(function() {
         bombo = new Bombo();
+        bombo.Iniciar();
         carton = new Carton();
         jugador = new Jugador();
+        jugador.NuevoCarton();
     });
 
 
     describe("El bombo...", function() {
+
+        it("Solo puede existir un bombo activo", function() {
+            expect(bombo.GetActivarBombo()).toBeTruthy();
+
+            var otroBombo;
+            otroBombo = new Bombo();
+            expect(otroBombo.GetActivarBombo()).toBeFalsy();
+
+        });
 
         it("Solo puede cantar un maximo de 90 numeros", function() {
             expect(bombo.GetContador()).toBe(90);
         });
 
         it("Debe saber que numeros se han cantado", function() {
-            expect(bombo.GetTablaCantados().length).toBe(90);
+            expect(bombo.GetTablaNumerosCantados().length).toBe(90);
         });
+
+        xit("El bombo debe notificar a los jugadores los numeros que van saliendo", function() {
+
+        })
 
     });
 
     describe("Cada jugador...", function() {
 
-        it("Debe tener un carton", function() {
-            jugador.MiCarton();
+        xit("Debe tener un carton", function() {
+
         });
     });
 
@@ -40,11 +53,11 @@ describe("En la Kata Bingo", function() {
         });
 
         it("Debe tener 12 casillas nulas", function() {
-            expect(carton.GetPosicionesPorTipo(0)).toBe(12);
+            expect(carton.GetTotalPosicionesPorTipo(0)).toBe(12);
         });
 
         it("Debe tener 15 casillas validas", function () {
-            expect(carton.GetPosicionesPorTipo(1)).toBe(15);
+            expect(carton.GetTotalPosicionesPorTipo(1)).toBe(15);
         });
 
         it("Cada columna tiene un rango de valores predeterminado", function() {
--- jasmine/src/Bingo.js
+++ jasmine/src/Bingo.js
@@ -1,91 +1,138 @@
-var Bombo = function() {
+var Bombo, Carton, Jugador;
 
-    var tablaNumeros = []
-    var tablaCantados = [];
+Bombo = function () {
 
-    var numeroMinimo = 1;
-    var numeroMaximo = 90;
-    var contador = 0;
+    var tablaNumeros, tablaNumerosCantados,
+        numeroMinimo, numeroMaximo,
+        contador,
+        activarBombo;
 
-    this.LlenarBombo = function() {
+    tablaNumeros = [];
+    tablaNumerosCantados = [];
+
+    numeroMinimo = 1;
+    numeroMaximo = 90;
+    contador = 0;
+    activarBombo = false;
+
+    this.LlenarBombo = function () {
         for (var i = numeroMinimo; i <= numeroMaximo; i++) {
             tablaNumeros.push(i);
         }
-    }
+    };
 
-    this.CantarNumero = function() {
-        var numero = this.GenerarNumeroRandom(numeroMinimo,numeroMaximo);
+    this.ComenzarJuego = function () {
+        for (var i = 0; this.GetContador() < numeroMaximo; i++) {
+            this.CantarNumero();
+        }
+    };
 
-        if (contador < numeroMaximo) {
-            if (tablaNumeros[numero-1] === numero) {
-                tablaNumeros[numero-1] = 0;
+    this.GetNumeroMinimo = function () {
+        return numeroMinimo;
+    };
+
+    this.GetNumeroMaximo = function () {
+        return numeroMaximo;
+    };
+
+    this.GetTablaNumeros = function () {
+        return tablaNumeros;
+    };
+
+    this.GetTablaNumerosCantados = function () {
+        return tablaNumerosCantados;
+    };
+
+    this.GetContador = function () {
+        return contador;
+    };
+
+    this.GetActivarBombo = function () {
+        return activarBombo;
+    };
+
+    this.SetActivarBombo = function (accion) {
+        activarBombo = !!((accion === true || accion === false));
+    };
+
+    this.SetContador = function () {
+        contador++;
+    };
+
+    this.SetNumerosCantados = function (numero) {
+        tablaNumerosCantados.push(numero);
+    };
+
+    this.CantarNumero = function () {
+        var numero = GenerarNumeroRandom(this.GetNumeroMinimo(), this.GetNumeroMaximo());
+
+        if (this.GetContador() < this.GetNumeroMaximo()) {
+            if (this.GetTablaNumeros()[numero - 1] === numero) {
+                this.GetTablaNumeros()[numero - 1] = 0;
                 this.SetContador();
                 this.SetNumerosCantados(numero);
+                //CheckNumero(numero);
             }
+        } else {
+            return false;
+        }
+    };
+
+    this.Iniciar = function () {
+        if (!(this.GetActivarBombo())) {
+            this.LlenarBombo();
+            this.ComenzarJuego();
+            this.SetActivarBombo(true);
         } else {
             return false;
         }
     }
 
-    this.ComenzarJuego = function() {
-        for (var i = 0; this.GetContador() < numeroMaximo; i++) {
-            this.CantarNumero();
-        }
-    }
 
-    this.GetTablaNumeros = function() {
-        return tablaNumeros;
-    }
 
-    this.GetTablaCantados = function() {
-        return tablaCantados;
-    }
-
-    this.GetContador = function() {
-        return contador;
-    }
-
-    this.SetContador = function() {
-        contador++;
-    }
-
-    this.SetNumerosCantados = function(numero) {
-        tablaCantados.push(numero);
-    }
-
-    //Arrancar bombo
-    this.LlenarBombo();
-    this.ComenzarJuego();
 };
 
+Carton = function () {
 
-var Carton = function() {
+    var cuadricula, posiciones,
+        rangoInicio, rangoFin,
+        numeroMaximoFilas, numeroMaximoColumnas,
+        numeroMaximoPosicionesNulas, numeroMaximoPosicionesValidas;
 
-    var cuadricula = [ [], [], [] ];
-    var posiciones = [ [], [], [] ];
+    cuadricula = [
+        [],
+        [],
+        []
+    ];
 
-    var rangoInicio = [1,10,20,30,40,50,60,70,80];
-    var rangoFin    = [9,19,29,39,49,59,69,79,90];
+    posiciones = [
+        [],
+        [],
+        []
+    ];
 
-    var numeroMaximoFilas = 3;
-    var numeroMaximoColumnas = 9;
+    rangoInicio = [1, 10, 20, 30, 40, 50, 60, 70, 80];
+    rangoFin = [9, 19, 29, 39, 49, 59, 69, 79, 90];
 
-    var numeroMaximoPosicionesNulas = 4;
-    var numeroMaximoPosicionesValidas = 5;
+    numeroMaximoFilas = 3;
+    numeroMaximoColumnas = 9;
 
-    this.GetCuadricula = function() {
+    numeroMaximoPosicionesNulas = 4;
+    numeroMaximoPosicionesValidas = 5;
+
+    this.GetCuadricula = function () {
         return cuadricula;
-    }
+    };
 
-    this.GetPosiciones = function(x, y) {
+    this.GetPosiciones = function (x, y) {
         return posiciones[x][y];
-    }
+    };
 
-    this.GetRango = function(tipo, posicion) {
+    this.GetRango = function (tipo, posicion) {
         return (tipo === 'inicio' || tipo === 'fin') ? rangoInicio[posicion] : rangoFin[posicion];
-    }
+    };
 
-    this.GetPosicionesPorTipo = function(tipo) {
+    this.GetTotalPosicionesPorTipo = function (tipo) {
         var contador = 0;
         for (var i = 0; i < numeroMaximoFilas; i++) {
             for (var j = 0; j < numeroMaximoColumnas; j++) {
@@ -93,9 +140,19 @@ var Carton = function() {
             }
         }
         return contador;
-    }
+    };
 
-    this.GenerarPosiciones = function() {
+    this.GetLinea = function (fila) {
+        var contador = 0;
+        for (var i = 0; i < cuadricula[fila].length; i++) {
+            if (posiciones[fila][i] === 1) {
+                contador++;
+            }
+        }
+        return contador;
+    };
+
+    this.GenerarPosiciones = function () {
         for (var i = 0; i < numeroMaximoFilas; i++) {
 
             var contadorPosicionesNulas = 0;
@@ -103,14 +160,13 @@ var Carton = function() {
 
             for (var j = 0; j < numeroMaximoColumnas; j++) {
 
-                var numeroRandom = this.GenerarNumeroRandom(0,1);
+                var numeroRandom = GenerarNumeroRandom(0, 1);
 
                 if (numeroRandom === 0) {
                     contadorPosicionesNulas++
                 } else {
                     contadorPosicionesValidas++
-                };
-
+                }
                 if (contadorPosicionesNulas <= numeroMaximoPosicionesNulas) {
                     if (contadorPosicionesValidas <= numeroMaximoPosicionesValidas) {
                         posiciones[i][j] = numeroRandom;
@@ -126,49 +182,102 @@ var Carton = function() {
                 }
             }
         }
-    }
+    };
 
-    this.GenerarValores = function() {
+    this.GenerarValores = function () {
         var numeroRandom = 0;
         for (var i = 0; i < numeroMaximoFilas; i++) {
             for (var j = 0; j < numeroMaximoColumnas; j++) {
                 if (this.GetPosiciones(i, j) === 1) {
-                    numeroRandom = this.GenerarNumeroRandom(this.GetRango('inicio', i), this.GetRango('fin', i));
+                    numeroRandom = GenerarNumeroRandom(this.GetRango('inicio', i), this.GetRango('fin', i));
                     this.SetValores(i, j, numeroRandom);
                 } else {
                     this.SetValores(i, j, 0);
                 }
             }
         }
-    }
+    };
 
 
-    this.SetValores = function(coordenadaX, coordenadaY, valor) {
+    this.SetValores = function (coordenadaX, coordenadaY, valor) {
         cuadricula[coordenadaX][coordenadaY] = valor;
-    }
+    };
 
     //Inicializar el carton con datos
     this.GenerarPosiciones();
     this.GenerarValores();
 
-}
+};
 
-var Jugador = function() {
-    this.MiCarton = function() {
-        var nuevoCarton;
-        nuevoCarton = new Carton();
+Jugador = function () {
+    var carton, tablaEncontrado, linea, bingo;
+
+    tablaEncontrado = [[],[],[]];
+    linea = 0;
+    bingo = false;
+
+    this.NumeroEncontrado = function(fila, columna, numero) {
+        tablaEncontrado[fila][columna] = numero;
+        this.CheckLinea(fila);
+        this.CheckBingo();
+    };
+
+    this.CheckLinea = function(fila) {
+        var linea = 0;
+        for (var i = 0; i < carton.length; i++) {
+            for (var j = 0; j < carton[i].length; j++) {
+                if (tablaEncontrado[i][j] === carton[i][j]) {
+                    linea++;
+                }
+                if (carton.GetLinea(fila) === linea) {
+                    this.GritarLinea();
+                }
+            }
+        }
+    };
+
+    this.GritarLinea = function() {
+        linea++;
+    };
+
+    this.CheckBingo = function() {
+        if (linea > 3) {
+            this.GritarBingo();
+        }
+    };
+
+    this.GritarBingo = function() {
+        bingo = true;
+        this.Fin();
+    };
+
+    this.NuevoCarton = function () {
+        carton = new Carton();
+        return carton;
+    };
+
+
+    this.Fin = function() {
+        return true;
     }
-}
 
+};
 
-var Commons = function()  {
-    this.GenerarNumeroRandom = function(inicio, fin) {
-
-        /*Random a number with Math Objects
-         https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Math/random*/
-        return Math.floor(Math.random() * (fin - inicio + 1)) + inicio;
+/*
+var CheckNumero = function(numero) {
+    for (var i = 0; i < carton.GetCuadricula().length; i++) {
+        for (var j = 0; j < carton.GetCuadricula()[i].length; j++) {
+            if (carton.GetCuadricula()[i][j] === numero) {
+                this.NumeroEncontrado(i, j, numero);
+            }
+        }
     }
-}
 
-Carton.prototype = new Commons();
-Bombo.prototype = new Commons();
+};
+*/
+
+var GenerarNumeroRandom = function (inicio, fin) {
+    /*Random a number with Math Objects
+     https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Math/random*/
+    return Math.floor(Math.random() * (fin - inicio + 1)) + inicio;
+};
